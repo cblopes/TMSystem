95409af5a42a5fa7fa2a21e956f666e6fe621bdc
Christhian Lopes <christhiancbl@gmail.com>
2024-10-06 14:14:06 -0300

    Define os métodos básicos das interfaces de serviço

---
+++ src/TMS.Business/Interfaces/IOcorrenciaService.cs
@@ -0,0 +1,10 @@
+﻿using TMS.Business.Entities;
+
+namespace TMS.Business.Interfaces;
+
+public interface IOcorrenciaService : IDisposable
+{
+    Task Adicionar(Ocorrencia ocorrencia);
+    Task Atualizar(Ocorrencia ocorrencia);
+    Task Remover(int id);
+}
---
+++ src/TMS.Business/Interfaces/IPedidoService.cs
@@ -0,0 +1,9 @@
+﻿using TMS.Business.Entities;
+
+namespace TMS.Business.Interfaces;
+
+public interface IPedidoService : IDisposable
+{
+    Task Adicionar(Pedido pedido);
+    Task Remover(int id);
+}
