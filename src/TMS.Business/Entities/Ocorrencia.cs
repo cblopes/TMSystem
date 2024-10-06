45783858df0b987ce0ba78c60c2851ebb9ae0bd3
Christhian Lopes <christhiancbl@gmail.com>
2024-10-06 19:18:57 -0300

    Adiciona mapeamento da entidade Pedido

--- src/TMS.Business/Entities/Ocorrencia.cs
+++ src/TMS.Business/Entities/Ocorrencia.cs
@@ -3,7 +3,11 @@
 public class Ocorrencia
 {
     public int IdOcorrencia { get; set; }
+    public int IdPedido { get; set; }
     public TipoOcorrencia TipoOcorrencia { get; set; }
     public DateTime HoraOcorrencia { get; set; }
     public bool IndFinalizadora { get; set; }
+
+    /* EF Relation */
+    public Pedido? Pedido { get; set; }
 }
---
+++ src/TMS.Data/Mappings/PedidoMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TMS.Business.Entities;
+
+namespace TMS.Data.Mappings;
+
+public class PedidoMapping : IEntityTypeConfiguration<Pedido>
+{
+    public void Configure(EntityTypeBuilder<Pedido> builder)
+    {
+        builder.HasKey(x => new { x.IdPedido, x.NumeroPedido });
+
+        builder.HasMany(x => x.Ocorrencias)
+            .WithOne(x => x.Pedido)
+            .HasForeignKey(x => x.IdPedido);
+
+        builder.ToTable("Pedidos");
+    }
+}
