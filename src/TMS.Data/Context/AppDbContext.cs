8b040a7bd9c09035719e085f45f8d67d39dd6e37
Christhian Lopes <christhiancbl@gmail.com>
2024-10-06 20:33:31 -0300

    Adiciona configurações no DbContext

--- src/TMS.Data/Context/AppDbContext.cs
+++ src/TMS.Data/Context/AppDbContext.cs
@@ -7,9 +7,25 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
-        
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
     }
 
     public DbSet<Pedido> Pedidos { get; set; }
     public DbSet<Ocorrencia> Ocorrencias { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+            .SelectMany(x => x.GetProperties()
+                .Where(x => x.ClrType == typeof(string))))
+            property.SetColumnType("VARCHAR(100)");
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys()))
+            relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
