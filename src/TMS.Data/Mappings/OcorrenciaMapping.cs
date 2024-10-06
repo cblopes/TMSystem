8199f0a2111be5def26551a73d85061cbd1a543f
Christhian Lopes <christhiancbl@gmail.com>
2024-10-06 19:37:18 -0300

    Adiciona mapeamento da entidade Ocorrencia

---
+++ src/TMS.Data/Mappings/OcorrenciaMapping.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TMS.Business.Entities;
+
+namespace TMS.Data.Mappings;
+
+public class OcorrenciaMapping : IEntityTypeConfiguration<Ocorrencia>
+{
+    public void Configure(EntityTypeBuilder<Ocorrencia> builder)
+    {
+        builder.HasKey(x => x.IdOcorrencia);
+
+        builder.Property(x => x.TipoOcorrencia)
+            .HasConversion<string>()
+            .HasColumnType("VARCHAR(20)")
+            .IsRequired();
+
+        builder.ToTable("Ocorrencias");
+    }
+}
