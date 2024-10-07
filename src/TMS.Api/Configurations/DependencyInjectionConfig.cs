9c6445c9dd02bc67c4cde522b8b49d0ed9dc95a3
Christhian Lopes <christhiancbl@gmail.com>
2024-10-07 00:34:21 -0300

    Adiciona configuração de injeção de dependência

---
+++ src/TMS.Api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using TMS.Business.Interfaces;
+using TMS.Business.Notificacoes;
+using TMS.Business.Services;
+using TMS.Data.Context;
+using TMS.Data.Repositories;
+
+namespace TMS.Api.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+    {
+        // Data
+        services.AddScoped<AppDbContext>();
+        services.AddScoped<IPedidoRepository, PedidoRepository>();
+        services.AddScoped<IOcorrenciaRepository, OcorrenciaRepository>();
+
+        // Business
+        services.AddScoped<INotificador, Notificador>();
+        services.AddScoped<IPedidoService, IPedidoService>();
+        services.AddScoped<IOcorrenciaService, OcorrenciaService>();
+
+        return services;
+    }
+}
--- src/TMS.Api/Program.cs
+++ src/TMS.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TMS.Api.Configurations;
 using TMS.Data.Context;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
