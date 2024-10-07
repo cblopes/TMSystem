63520b7e26d58c15bb92834ea2247a31c0ac13f7
Christhian Lopes <christhiancbl@gmail.com>
2024-10-06 21:42:42 -0300

    Adiciona implementação específica dos repositórios de pedido e ocorrência

---
+++ src/TMS.Data/Repositories/OcorrenciaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMS.Business.Entities;
+using TMS.Business.Interfaces;
+using TMS.Data.Context;
+
+namespace TMS.Data.Repositories;
+
+public class OcorrenciaRepository : Repository<Ocorrencia>, IOcorrenciaRepository
+{
+    public OcorrenciaRepository(AppDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<Ocorrencia>> ObterOcorrenciasPorPedido(int pedidoId)
+    {
+        return await Context.Ocorrencias.Where(x => x.IdPedido == pedidoId).ToListAsync();
+    }
+
+    public override async Task Remover(int id)
+    {
+        DbSet.Remove(new() { IdOcorrencia = id });
+        await SaveChanges();
+    }
+}
---
+++ src/TMS.Data/Repositories/PedidoRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TMS.Business.Entities;
+using TMS.Business.Interfaces;
+using TMS.Data.Context;
+
+namespace TMS.Data.Repositories;
+
+public class PedidoRepository : Repository<Pedido>, IPedidoRepository
+{
+    public PedidoRepository(AppDbContext context) : base(context) { }
+    public async Task<Pedido> ObterPedidoOcorrencias(int id)
+    {
+        return await Context.Pedidos.AsNoTracking()
+            .Include(x => x.Ocorrencias).FirstOrDefaultAsync();
+    }
+
+    public override async Task Remover(int id)
+    {
+        DbSet.Remove(new() { IdPedido = id });
+        await SaveChanges();
+    }
+}
